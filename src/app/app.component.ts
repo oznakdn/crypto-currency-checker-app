d0f2d8171098e73f8504151c80e9e776101c1c68
Ozan Akaydin <ozanakaydin@gmail.com>
2022-06-25 10:37:15 +0300

    Crypto Currency Checker project is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CurrencyService } from './service/currency.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crypto-currency-checker-app';
+
+  selectedCurrency:string="TRY";
+
+  constructor(private currencyService:CurrencyService){
+
+  }
+
+  sendCurrency(event:string){
+    console.log(event);
+    this.currencyService.setCurrency(event);
+  }
 }
